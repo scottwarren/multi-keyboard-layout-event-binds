c9383aecc531e9fa3b6a9a837e8bd38ff68fc32c
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-07-30 11:30:03 +1000

    chore: vite init install commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">
+        Multi keyboard layout binds
+      </h1>
+      <p>
+        Try it out! Press <code>mod+shift+a</code> to see the magic happen!
+      </p>
+    </div>
+  )
+}
+
+export default App
