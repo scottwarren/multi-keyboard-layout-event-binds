40208eb0cfe20d736db1853d16e1f45e8d6180f3
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-08-07 12:59:08 +1000

    fix: fixes keybind not working properly
    was being re-bound upon every render

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { useCallback, useState } from 'react'
+import { useGlobalKeyBind } from './hooks/useGlobalKeyBind'
+
 function App() {
+  const [showMessage, setShowMessage] = useState(false)
+
+  useGlobalKeyBind('k', () => {
+    console.log('hello!', 'k', 'pressed')
+    setShowMessage((m) => !m)
+  })
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-center">
         Multi keyboard layout binds
       </h1>
-      <p>
-        Try it out! Press <code>mod+shift+a</code> to see the magic happen!
-      </p>
+      {!showMessage && (
+        <p>
+          Try it out! Press <code>k</code> to see the magic happen!
+        </p>
+      )}
+      {showMessage && <p>hello! k pressed</p>}
     </div>
   )
 }
--- src/hooks/useGlobalKeyBind.ts
+++ src/hooks/useGlobalKeyBind.ts
@@ -1,31 +1,35 @@
-import { useCallback } from 'react'
+import { useEffect } from 'react'
 
 /**
  * Used to bind a key to a callback function on a global level.
  */
-export function useGlobalKeyBind({ key, callback }: ElementKeyBindProps) {
-  const handler = useCallback(
-    (e: KeyboardEvent) => {
-      // TODO: add parsing of key combinations
-      // TODO: add support for checking whether certain keys are pressed.
-      // TODO: Test which method of checking for key combinations works amongst multiple keyboard layouts.
-      if (e.key === key) {
-        callback(e)
-      }
-    },
-    [key, callback],
-  )
+export function useGlobalKeyBind(
+  // Key to bind to the callback function.
+  key: string,
+  // Callback to run when the above key is pressed.
+  callback: (ev: KeyboardEvent) => void,
+) {
+  const handler = handleKeyDown(key, callback)
 
-  document.addEventListener('keydown', handler)
+  useEffect(() => {
+    document.addEventListener('keypress', handler)
 
-  return () => {
-    document.removeEventListener('keydown', handler)
-  }
+    return () => {
+      document.removeEventListener('keypress', handler)
+    }
+  }, [handler])
 }
 
-interface ElementKeyBindProps {
-  /** Key(s) for the event */
-  key: string
-  /** Callback to be run upon the key press occurring. */
-  callback: (ev: KeyboardEvent) => void
+/**
+ * Used to create the keydown handler function
+ */
+function handleKeyDown(key: string, callback: (ev: KeyboardEvent) => void) {
+  return function keydown(e: KeyboardEvent) {
+    // TODO: add parsing of key combinations
+    // TODO: add support for checking whether certain keys are pressed.
+    // TODO: Test which method of checking for key combinations works amongst multiple keyboard layouts.
+    if (e.key === key) {
+      callback(e)
+    }
+  }
 }
