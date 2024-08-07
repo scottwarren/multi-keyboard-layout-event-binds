4c85255c4f548e8fc6fd9e09591818531a6999c4
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-08-07 13:08:00 +1000

    fix: fixes happy-dom installation for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'happy-dom',
-  },
   resolve: {
     alias: {
       '@/*': './src/*',
