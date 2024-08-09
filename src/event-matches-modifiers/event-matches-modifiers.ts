fb7303fe191394a0103f6be2c317c88083046d12
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2024-08-09 14:34:34 +1000

    feat: implements event matches modifiers check

--- src/event-matches-modifiers/event-matches-modifiers.ts
+++ src/event-matches-modifiers/event-matches-modifiers.ts
@@ -1,8 +1,31 @@
 import type { ParsedKeyT } from '../parse-key-string/parse-key-string.types'
 
+import pick from 'lodash/pick'
+import isEqual from 'lodash/isEqual'
+
 /**
+ * Checks whether the event modifier keys are pressed match the parsed key (i.e. the keys that were bound).
  *
  * @returns {Boolean} true if and only if the modifier keys within the parsed key _all_
  */
-export function eventMatchesModifiers(e: KeyboardEvent, parsedKey: ParsedKeyT) {
+export function eventMatchesModifiers(
+  event: KeyboardEvent,
+  parsedKey: ParsedKeyT,
+) {
+  const modifierKeys = pick(parsedKey, [
+    'altKey',
+    'ctrlKey',
+    'metaKey',
+    'shiftKey',
+  ])
+
+  const eventModifierKeys = pick(event, [
+    'altKey',
+    'ctrlKey',
+    'metaKey',
+    'shiftKey',
+  ])
+
+  // TODO: performance test this function, as this will be running on every keypress.
+  return isEqual(modifierKeys, eventModifierKeys)
 }
